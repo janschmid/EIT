d719570fc0697068570dd16e622a3dba52ebb4ae
Alexander Sokolovs <alsok20@student.sdu.dk>
2021-11-24 16:57:27 +0100

    Positioning, rotation working

--- eit_playground/src/offb_node_guidance.cpp
+++ eit_playground/src/offb_node_guidance.cpp
@@ -51,6 +51,8 @@ void cam_pos_cb(geometry_msgs::PoseStamped msg){
 	camOrient[2] = msg.pose.orientation.z;
 	camOrient[3] = msg.pose.orientation.w;
 
+	arucoQuaternion = tf2::Quaternion(camOrient[0], camOrient[1], camOrient[2], camOrient[3]);	
+
 	if(msg.header.frame_id ==  "aruco_marker"){
 		frameSeen = true;
 		mission=false;
@@ -77,10 +79,202 @@ void cam_rotation(double deg, double *vect){
 	}
 }
 
+// For matrix mathematics
+void getCofactor(double mat[N][N], double temp[N][N], int p, int q, int n){
+    int i = 0, j = 0;
+    // Looping for each element of the matrix
+    for (int row = 0; row < n; row++){
+        for (int col = 0; col < n; col++){
+            //  Copying into temporary matrix only those
+            //  element which are not in given row and
+            //  column
+            if (row != p && col != q){
+                temp[i][j++] = mat[row][col];
+                // Row is filled, so increase row index and
+                // reset col index
+                if (j == n - 1){
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+    }
+}
+ 
+/* Recursive function for finding determinant of matrix.
+   n is current dimension of mat[][]. */
+double determinant(double mat[N][N], int n){
+    double D = 0; // Initialize result
+    //  Base case : if matrix contains single element
+    if (n == 1){
+        return mat[0][0];
+	}
+    double temp[N][N]; // To store cofactors
+    int sign = 1; // To store sign multiplier
+    // Iterate for each element of first row
+    for (int f = 0; f < n; f++){
+        // Getting Cofactor of mat[0][f]
+        getCofactor(mat, temp, 0, f, n);
+        D += sign * mat[0][f] * determinant(temp, n - 1);
+        // terms are to be added with alternate sign
+        sign = -sign;
+    }
+    return D;
+}
+// End for matrix calculations
+
+//Calculate the altitude with a spline
+double getZ(double t, double a[4]){
+	return (1*a[0]+t*a[1]+t*t*a[2]+t*t*t*a[3]);
+}
+
+void altitudeControlThread(double *localPos, double *camPos, double zEnd, ros::ServiceClient sm, geometry_msgs::PoseStamped *waypoint){
+	double t = 10;
+	double B[4][4] = {{1,0,0,0},{0,1,0,0},{1,t,t*t,t*t*t},{0,1,2*t,3*t*t}};
+	double BA11 = B[1][1]*B[2][2]*B[3][3]+B[1][2]*B[2][3]*B[3][1]+B[1][3]*B[2][1]*B[3][2] -B[1][3]*B[2][2]*B[3][1]-B[1][2]*B[2][1]*B[3][3]-B[1][1]*B[2][3]*B[3][2];
+	double BA12 = -B[0][1]*B[2][2]*B[3][3]-B[0][2]*B[2][3]*B[3][1]-B[0][3]*B[2][1]*B[3][2] +B[0][3]*B[2][2]*B[3][1]+B[0][2]*B[2][1]*B[3][3]+B[0][1]*B[2][3]*B[3][2];
+	double BA13 = B[0][1]*B[1][2]*B[3][3]+B[0][2]*B[1][3]*B[3][1]+B[0][3]*B[1][1]*B[3][2] -B[0][3]*B[1][2]*B[3][1]-B[0][2]*B[1][1]*B[3][3]-B[0][1]*B[1][3]*B[3][2];
+	double BA14 = -B[0][1]*B[1][2]*B[2][3]-B[0][2]*B[1][3]*B[2][1]-B[0][3]*B[1][1]*B[2][2] +B[0][3]*B[1][2]*B[2][1]+B[0][2]*B[1][1]*B[2][3]+B[0][1]*B[1][3]*B[3][3];
+	
+	double BA21 = -B[1][0]*B[2][2]*B[3][3]-B[1][2]*B[2][3]*B[3][0]-B[1][3]*B[2][0]*B[3][2] +B[1][3]*B[2][2]*B[3][0]+B[1][2]*B[2][0]*B[3][3]+B[1][0]*B[2][3]*B[3][2];
+	double BA22 = B[0][0]*B[2][2]*B[3][3]+B[0][2]*B[2][3]*B[3][0]+B[0][3]*B[2][0]*B[3][2] -B[0][3]*B[2][2]*B[3][0]-B[0][2]*B[2][0]*B[3][3]-B[0][0]*B[2][3]*B[3][2];
+	double BA23 = -B[0][0]*B[1][2]*B[3][3]-B[0][2]*B[1][3]*B[3][0]-B[0][3]*B[1][0]*B[3][2] +B[0][3]*B[1][2]*B[3][0]+B[0][2]*B[1][0]*B[3][3]+B[0][0]*B[1][3]*B[3][2];
+	double BA24 = B[0][0]*B[1][2]*B[2][3]+B[0][2]*B[1][3]*B[2][1]+B[0][3]*B[1][0]*B[2][2] -B[0][3]*B[1][2]*B[2][1]-B[0][2]*B[1][0]*B[2][3]-B[0][0]*B[1][3]*B[2][2];
+	
+	double BA31 = B[1][0]*B[2][1]*B[3][3]+B[1][1]*B[2][3]*B[3][0]+B[1][3]*B[2][0]*B[3][1] -B[1][3]*B[2][1]*B[3][0]-B[1][1]*B[2][0]*B[3][3]-B[1][0]*B[2][3]*B[3][1];
+	double BA32 = -B[0][0]*B[2][1]*B[3][3]-B[0][1]*B[2][3]*B[3][0]-B[0][3]*B[2][0]*B[3][1] +B[0][3]*B[2][1]*B[3][0]+B[0][1]*B[2][0]*B[3][3]+B[0][0]*B[2][3]*B[3][1];
+	double BA33 = B[0][0]*B[1][1]*B[3][3]+B[0][1]*B[1][3]*B[3][0]+B[0][3]*B[1][0]*B[3][1] -B[0][3]*B[1][1]*B[3][0]-B[0][1]*B[1][0]*B[3][3]-B[0][0]*B[1][3]*B[3][1];
+	double BA34 = -B[0][0]*B[1][1]*B[2][3]-B[0][1]*B[1][3]*B[2][0]-B[0][3]*B[1][0]*B[2][1] +B[0][3]*B[1][1]*B[2][0]+B[0][1]*B[1][0]*B[2][3]+B[0][0]*B[1][3]*B[2][1];
+	
+	double BA41 = -B[1][0]*B[2][1]*B[3][2]-B[1][1]*B[2][2]*B[3][0]-B[1][2]*B[2][0]*B[3][1] +B[1][2]*B[2][1]*B[3][0]+B[1][1]*B[2][0]*B[3][2]+B[1][0]*B[2][2]*B[3][1];
+	double BA42 = B[0][0]*B[2][1]*B[3][2]+B[0][1]*B[2][2]*B[3][0]+B[0][2]*B[2][0]*B[3][1] -B[0][2]*B[2][1]*B[3][0]-B[0][1]*B[2][0]*B[3][2]-B[0][0]*B[2][2]*B[3][1];
+	double BA43 = -B[0][0]*B[1][1]*B[3][2]-B[0][1]*B[1][2]*B[3][0]-B[0][2]*B[1][0]*B[3][1] +B[0][2]*B[1][1]*B[3][0]+B[0][1]*B[1][0]*B[3][2]+B[0][0]*B[1][2]*B[3][1];
+	double BA44 = B[0][0]*B[1][1]*B[2][2]+B[0][1]*B[1][2]*B[2][0]+B[0][2]*B[1][0]*B[2][1] -B[0][2]*B[1][1]*B[2][1]-B[0][1]*B[1][0]*B[2][2]-B[0][0]*B[1][2]*B[2][1];
+	double BAdjuncate[4][4] = {{BA11, BA12, BA13, BA14},{BA21, BA22, BA23, BA24},{BA31, BA32, BA33, BA34},{BA41, BA42, BA43, BA44}};
+
+	double pInvB[4][4];
+	double d = determinant(B, 4);
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			pInvB[i][j] = (1/d)*BAdjuncate[i][j];
+		}
+	}
+	double alpha[4];
+	double initVal[4] = {localPos[2],0,zEnd,0};	//Starting altitude + velocity + ending altitude + velocity
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			alpha[i] += pInvB[i][j]*initVal[j];
+		}
+	}
+	
+	(*waypoint).pose.position.z = localPos[2];	//I know I know this is not necessary...
+	double zNow = localPos[2];
+	double timer = 0;
+	clock_t startTime = clock();	//Start timer
+	while(running){
+		//std::cout << std::abs(camPos[0]+camPos[1]) << "Campos : \n";
+		if(std::abs(camPos[0]+camPos[1]) < 0.05 && frameSeen == true){
+			//std::cout << localPos[2]-zNow << "\n";
+			if(std::abs(localPos[2]-zNow) <= 0.10 & (clock()-startTime)/CLOCKS_PER_SEC > TIMING){
+				timer += 1.0;
+				zNow = getZ(timer, alpha);
+				(*waypoint).pose.position.z = zNow;	//I know I know this is not necessary...
+				startTime = clock();
+				std::cout << zNow << " landing\n";
+				std::cout << localPos[2] << " Current altitude\n";
+			}
+		}	
+		if(localPos[2] <= zEnd+0.05 || timer > 10){
+			//Let thread end when landing occurs
+			std::cout << "End landing\n";
+			running = false;
+    		mavros_msgs::SetMode land_set_mode;
+    		land_set_mode.request.custom_mode = "AUTO.LAND";
+			sm.call(land_set_mode); 
+			threadSaysLandingEnded = true;
+			break;
+		}
+	}
+}
+
+void mulQuaternion(double *q2, double *q1, double *qret){
+	double q[4];
+	qret[0] = q[0]*q2[0]-q1[1]*q2[1]-q1[2]*q2[2]-q1[3]*q2[3];
+	qret[1] = q[0]*q2[1]+q1[1]*q2[0]+q1[2]*q2[3]-q1[3]*q2[2];
+	qret[2] = q[0]*q2[2]-q1[1]*q2[3]+q1[2]*q2[0]+q1[3]*q2[1];
+	qret[3] = q[0]*q2[3]-q1[1]*q2[2]-q1[2]*q2[1]-q1[3]*q2[0];
+}
+
+void invQuaternion(double *q){
+	q[3] = q[3]*-1;
+}
+
+void normQuaternion(double *q){
+	double mag = sqrt(pow(q[0],2)+pow(q[1],2)+pow(q[2],2)+pow(q[3],2));
+	q[0] /= mag;
+	q[1] /= mag;
+	q[2] /= mag;
+	q[3] /= mag;
+}
+
+void orintControl(geometry_msgs::PoseStamped *waypoint, double *curOrient, double *camOrient){
+	double qRet[4] = {0,0,0,0};
+	double result[4];
+
+	invQuaternion(curOrient);
+	mulQuaternion(camOrient,curOrient,qRet);
+	invQuaternion(curOrient);
+	mulQuaternion(qRet,curOrient,result);
+	normQuaternion(result);
+	
+	(*waypoint).pose.orientation.x = result[0];
+	(*waypoint).pose.orientation.y = result[1];
+	(*waypoint).pose.orientation.z = result[2];
+	(*waypoint).pose.orientation.w = result[3];
+	/*
+	double QR[4];
+	double Q_target[4];
+	//QR[0] = qRet[0]*curOrient[0];
+	//QR[1] = camOrient[1]*curOrient[1];
+	//QR[2] = camOrient[2]*curOrient[2];
+	//QR[3] = camOrient[3]*-curOrient[3];
+	QR[0] = camOrient[0]*curOrient[0];
+	QR[1] = camOrient[1]*curOrient[1];
+	QR[2] = camOrient[2]*curOrient[2];
+	QR[3] = camOrient[3]*-curOrient[3];
+
+	Q_target[0] = QR[0]*curOrient[0];
+	Q_target[1] = QR[1]*curOrient[1];
+	Q_target[2] = QR[2]*curOrient[2];
+	Q_target[3] = QR[3]*curOrient[3];
+
+	double mag = sqrt(pow(Q_target[0],2)+pow(Q_target[1],2)+pow(Q_target[2],2)+pow(Q_target[3],2));
+	
+	(*waypoint).pose.orientation.x = (Q_target[0]/mag);
+	(*waypoint).pose.orientation.y = (Q_target[1]/mag);
+	(*waypoint).pose.orientation.z = (Q_target[2]/mag);
+	(*waypoint).pose.orientation.w = (Q_target[3]/mag);
+	*/
+	std::cout <<"-----------------------\n";
+	std::cout << "camOrient: " << camOrient[0] << " " << camOrient[1] << " " << camOrient[2] << " " << camOrient[3] << "\n";
+	std::cout << "curOrient: " << curOrient[0] << " " << curOrient[1] << " " << curOrient[2] << " " << curOrient[3] << "\n";
+	//std::cout << "new rotation: " << QR[0] << " " << QR[1] << " " << QR[2] << " " << QR[3] << "\n";
+	
+}
 
 void posControl(geometry_msgs::PoseStamped *waypoint, double *curPos, double fX, double fY){
 	(*waypoint).pose.position.x = (curPos[0]+fY);
 	(*waypoint).pose.position.y = (curPos[1]+fX);
+}
+
+void altControl(geometry_msgs::PoseStamped *waypoint, double *curPos, double *camPos, ros::ServiceClient sm){
+	(*waypoint).pose.position.z = curPos[2]-10;
+	if(camPos[2] <= 0.5){
+    	mavros_msgs::SetMode land_set_mode;
+    	land_set_mode.request.custom_mode = "AUTO.LAND";
+		sm.call(land_set_mode);
+		threadSaysLandingEnded = true;	
+	}
 }
 
 int main(int argc, char **argv){
@@ -190,13 +384,25 @@ int main(int argc, char **argv){
 		
 		// Run guided landing with spline if marker is seen. 
 		}else{
+			targetWaypoint.pose.position.z = 1;
 			posControl(&targetWaypoint, localPos, camPos[0], camPos[1]);
-			targetWaypoint.pose.position.z = 2;
-			local_pos_pub.publish(targetWaypoint);
-			std::cout << targetWaypoint.pose.position.z <<"\n";
+
+			orintControl(&targetWaypoint, localOrient, camOrient);
 			
+			//cam_rotation(degree, camPos);
+			//altControl(&targetWaypoint, localPos, camPos, set_mode_client);	
+			/*if(startLanding == true){
+				
+				landingThread = std::thread(altitudeControlThread, localPos, camPos, 0.0, set_mode_client, &targetWaypoint);
+				startLanding = false;
+			}*/
+			local_pos_pub.publish(targetWaypoint);
 		}
 		
+		if(threadSaysLandingEnded == true){
+			landingThread.join();
+			break;
+		}
 		ros::spinOnce();
 		rate.sleep();
     //End og game-loop
