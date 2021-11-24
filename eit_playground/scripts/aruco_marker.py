Aruco marker returns now "global pos" of image Aruco marker bug fix: return quaternions

eit_playground/scripts/aruco_marker.py
@@ -5,7 +5,8 @@ from cv2 import aruco
 import numpy as np
 import sys
 from collections import defaultdict
-
+import math
+# from tf.transformations import quaternion_from_euler
 
 class ArucoMarker():
     aruco_dict = None
@@ -134,20 +135,33 @@ class ArucoMarker():
         frame, rotationVector, translationVector, ids = self.print_distance_to_aruco_marker(frame, markerLength, markerPostfixName)
         return frame, rotationVector, translationVector, ids
 
+    def get_global_pos_and_euler_angles(self, frame, markerLength, markerPostfixName):
+        frame, rotationVector, translationVector, ids = self.print_distance_to_aruco_marker(frame, markerLength, markerPostfixName)
+        eulerAngle=corrected_pos=None
+        if(len(rotationVector)>=1):
+            eulerAngle = ArucoMarker.rotationVectorToEulerAngles(rotationVector[0][0][0])
+            corrected_pos = ArucoMarker.correct_rotation_matrix(translationVector[0][0][0],eulerAngle[0])
+            corrected_pos.append(translationVector[0][0][0][2])
+        return eulerAngle, corrected_pos
+
 
     def tutorial_03_aruco_marker_pose_estimation_camera_feed(self):
         import time
         video = cv2.VideoCapture(0)
         # time.sleep(2.0)
-
+        arucoMarker = ArucoMarker()
         while True:
             ret, frame = video.read()
 
             if not ret:
                 break
+            eulerAngle, corrected_global_position = arucoMarker.get_global_pos_and_euler_angles(frame, 0.07, "laptop")
+            if (eulerAngle is not None and
+                corrected_global_position is not None and
+                len(eulerAngle)>0 and len(corrected_global_position)>0):
+                print("{0}, {1}".format(eulerAngle,corrected_global_position))
             
-            output,_,_,_= self.print_distance_to_aruco_marker(frame, 0.1, "simulation")
-            cv2.imshow('Estimated Pose', output)
+            cv2.imshow('Estimated Pose', frame)
 
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
@@ -156,6 +170,54 @@ class ArucoMarker():
         video.release()
         cv2.destroyAllWindows()
 
+    def rotate(origin, point, angle):
+        """
+        Rotate a point counterclockwise by a given angle around a given origin.
+
+        The angle should be given in radians.
+        """
+        ox, oy = origin
+        px, py = point
+
+        qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+        qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+        return qx, qy
+
+    def correct_rotation_matrix(translationVector, angle):
+        origin = [0,0]
+        if(len(translationVector)==3):
+            qx, qy = ArucoMarker.rotate(origin, translationVector[0:2],angle)
+            return [qx,qy]
+        return None
+            # print("rotation:{1}, Translation: {0}, eulerAngle: {2}".format(translationVector, (qx,qy), eulerAngle[2]))
+            # print(qx, qy)
+    
+    def isRotationMatrix(R) :
+
+        Rt = np.transpose(R)
+        shouldBeIdentity = np.dot(Rt, R)
+        I = np.identity(3, dtype = R.dtype)
+        n = np.linalg.norm(I - shouldBeIdentity)
+        return n < 1e-6
+	 
+    def rotationVectorToEulerAngles(R):
+        R, _ = cv2.Rodrigues(R)
+        if not (ArucoMarker.isRotationMatrix(R)):
+            return
+        sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+
+        singular = sy < 1e-6
+
+        if  not singular :
+            x = math.atan2(R[2,1] , R[2,2])
+            y = math.atan2(-R[2,0], sy)
+            z = math.atan2(R[1,0], R[0,0])
+        else :
+            x = math.atan2(-R[1,2], R[1,1])
+            y = math.atan2(-R[2,0], sy)
+            z = 0
+
+        return np.array([x, y, z])
 
 if __name__ == "__main__":
     arucoMarker = ArucoMarker()

eit_playground/scripts/aruco_pose_estimation.py
@@ -11,6 +11,7 @@ from cv_bridge import CvBridge
 from geometry_msgs.msg import PoseStamped
 from aruco_marker import ArucoMarker
 import numpy as np
+import math
 
 from tf.transformations import quaternion_from_euler
 
@@ -47,8 +48,6 @@ class ArucoPoseEstimatorNode:
 
         self.current_image = np.zeros([480, 640, 3])
         self.frame = 0
-        self.rotationVector = 0
-        self.translationVector = 0
         
         if(rospy.get_param("SIMULATION")==True):
             self.markerPostfixName = "simulation"
@@ -73,31 +72,41 @@ class ArucoPoseEstimatorNode:
     def cb_image(self, image):
         self.current_image = self.bridge.imgmsg_to_cv2(image, desired_encoding='passthrough')
 
-
     def estimate_pose(self):
         while not rospy.is_shutdown():
-            self.frame, self.rotationVector, self.translationVector, self.ids = self.arucoMarker.tutorial_03_aruco_marker_pose_estimation(
-                self.current_image.astype('uint8'), self.markerLength, self.markerPostfixName)
-            #for x in range(len(self.ids)):
-            #print(self.ids)
-            if self.ids is not None: 
-                #print(self.translationVector[0][0][0][0])
-                q = quaternion_from_euler(self.rotationVector[0][0][0][0], self.rotationVector[0][0][0][1], self.rotationVector[0][0][0][2])
-                #print(q)
-                self.aruco_pose_msg.header.frame_id = "aruco_marker"
-                self.aruco_pose_msg.header.stamp = rospy.Time.now()
-                self.aruco_pose_msg.pose.position.x = self.translationVector[0][0][0][0]
-                self.aruco_pose_msg.pose.position.y = self.translationVector[0][0][0][1]
-                self.aruco_pose_msg.pose.position.z = self.translationVector[0][0][0][2]
+            # self.frame, self.rotationVector, self.translationVector, self.ids = self.arucoMarker.tutorial_03_aruco_marker_pose_estimation(
+            #     self.current_image.astype('uint8'), self.markerLength, self.markerPostfixName)
+            # #for x in range(len(self.ids)):
+            # #print(self.ids)
+            # if self.ids is not None: 
+            #     #print(self.translationVector[0][0][0][0])
+            #     q = quaternion_from_euler(self.rotationVector[0][0][0][0], self.rotationVector[0][0][0][1], self.rotationVector[0][0][0][2])
+            #     #print(q)
+            #     self.aruco_pose_msg.header.stamp = rospy.Time.now()
+            #     self.aruco_pose_msg.pose.position.x = self.translationVector[0][0][0][0]
+            #     self.aruco_pose_msg.pose.position.y = self.translationVector[0][0][0][1]
+            #     self.aruco_pose_msg.pose.position.z = self.translationVector[0][0][0][2]
+            #     self.aruco_pose_msg.pose.orientation.x = q[0]
+            #     self.aruco_pose_msg.pose.orientation.y = q[1]
+            #     self.aruco_pose_msg.pose.orientation.z = q[2]
+            #     self.aruco_pose_msg.pose.orientation.w = q[3]
+
+            #     self.aruco_pos_pub.publish(self.aruco_pose_msg)
+            eulerAngle, corrected_global_position = self.arucoMarker.get_global_pos_and_euler_angles(self.current_image.astype('uint8'), self.markerLength, self.markerPostfixName)
+
+            if (eulerAngle is not None and
+                corrected_global_position is not None and
+                len(eulerAngle)>0 and len(corrected_global_position)>0):
+                self.aruco_pose_msg.pose.position.x = corrected_global_position[0]
+                self.aruco_pose_msg.pose.position.y = corrected_global_position[1]
+                self.aruco_pose_msg.pose.position.z = corrected_global_position[2]
+                q = quaternion_from_euler(eulerAngle[0], eulerAngle[1], eulerAngle[2])
                 self.aruco_pose_msg.pose.orientation.x = q[0]
                 self.aruco_pose_msg.pose.orientation.y = q[1]
                 self.aruco_pose_msg.pose.orientation.z = q[2]
                 self.aruco_pose_msg.pose.orientation.w = q[3]
-
                 self.aruco_pos_pub.publish(self.aruco_pose_msg)
-                
             else:
-                self.aruco_pose_msg.header.frame_id = "no_aruco_marker"
                 self.aruco_pose_msg.header.stamp = rospy.Time.now()
                 self.aruco_pose_msg.pose.position.x = 0
                 self.aruco_pose_msg.pose.position.y = 0

eit_playground/scripts/calibration.py
@@ -80,6 +80,6 @@ if __name__ == '__main__':
     print(mtx)
     print(dist)
     import os.path as path
-    folderPath = path.join(path.dirname(path.realpath(__file__)))
+    folderPath = path.join(path.dirname(path.realpath(__file__)), "calibrationFiles")
     np.save(path.join(folderPath, "calibration_matrix_piCam"), mtx)
     np.save(path.join(folderPath, "distortion_coefficients_piCam"), dist)